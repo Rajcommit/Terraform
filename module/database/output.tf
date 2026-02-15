6b75c59024d13fe3ec470d73d75f8dc71332452c
Rajcommit <raj.vbeyond@gmail.com>
2026-02-16 01:48:12 +0530

    enhance database outputs with descriptions and add rds endpoint/port outputs

--- module/database/output.tf
+++ module/database/output.tf
@@ -1,7 +1,25 @@
 output "dbusername" {
-    value = aws_db_instance.mysql.username
+    description = "Database username"
+    value       = var.db_username
 }
 
 output "dbpassword" {
-  value = aws_db_instance.mysql.password
+    description = "Database password"
+    value       = var.db_password
+    sensitive   = true
+}
+
+output "rds_endpoint" {
+    description = "RDS MYSQL PORT"
+    value = aws_db_instance.mysql.endpoint
+}
+
+output "rds_port" {
+    description = "RDS Mysql port"
+    value = aws_db_instance.mysql.port
+}
+
+output "db_username" {
+    description = "Database username"
+    value = var.db_username
 }
