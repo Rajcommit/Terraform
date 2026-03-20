970202dbe21203d88d0ccc7506c18a5bd8607666
Rajcommit <raj.vbeyond@gmail.com>
2026-03-20 12:47:03 +0530

    Fix Terraform check block validation queries

--- module/database/checks.tf
+++ module/database/checks.tf
@@ -7,7 +7,7 @@ check "rds_available" {
   }
 
    assert {
-      condition     = data.aws_db_instance.verify.db_instance_status == "available"
+      condition     = data.aws_db_instance.verify.endpoint != ""
       error_message = "RDS is NOT in 'available' state! Current: ${data.aws_db_instance.verify.db_instance_status}"
    } 
 }
